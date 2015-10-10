d92c378abd347663597ff4b0485d781aa477c2c6
shokei125 <shokei125@gmail.com>
2015-10-10 14:53:04 +0900

    build.gradle 修正2

--- app/src/main/java/com/example/student/study/MainActivity.java
+++ app/src/main/java/com/example/student/study/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import org.apache.commons.lang3.StringUtils;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,9 +12,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
-//test
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
